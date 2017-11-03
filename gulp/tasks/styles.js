4b058dfe4ce10c7abda1afe082f701901a207221
David <david.windler@windowslive.com>
2017-11-04 00:44:35 +0100

    responsive images

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,10 +4,11 @@ const autoPrefixer = require('autoprefixer');
 const cssVars = require('postcss-simple-vars');
 const cssNested = require('postcss-nested');
 const cssImport = require('postcss-import');
+const mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-             .pipe(postCss([cssImport, cssVars, cssNested, autoPrefixer]))
+             .pipe(postCss([cssImport, mixins, cssVars, cssNested, autoPrefixer]))
              .on('error', function(errorInfo) {
                console.error(errorInfo.toString());
                this.emit('end');
